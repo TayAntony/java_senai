8267d287b5b5578dc927b0f18a43e5c78014f8b6
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2022-09-22 13:16:30 -0300

    Delete java/Projeto06/src/funçãoNascimento directory

--- java/Projeto06/src/funçãoNascimento/Nascimento.java
+++
@@ -1,9 +0,0 @@
-package funçãoNascimento;
-
-import java.util.Scanner;
-
-public class Nascimento {
-    void idade (int atual, int nasc){
-        System.out.println("Você tem "+ (atual-nasc) + " anos");
-    }
-}
